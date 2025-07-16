1bf165164bf6ecb3edd7a7fdcf1d773696d91945
Brian <andriamanesybrian@gmail.com>
2025-07-17 01:05:44 +0300

    fin13

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -1,6 +1,18 @@
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_print_alphabet(void)
 {
-	write (1, "abcdefghijklmnopqrstuvwxyz", 26);
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		ft_putchar(a);
+		a++;
+	}
 }
